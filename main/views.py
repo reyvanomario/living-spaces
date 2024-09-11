00c777394e2f50a39c0874fc01f190771382472d
reyvanomario <reyvanomario24122004@gmail.com>
2024-09-11 21:20:21 +0700

    Membuat form untuk menambahkan produk dan membuat 4 fungsi untuk melihat objek yang sudah ditambahkan dalam format XML, JSON, XML by ID, dan JSON by ID

--- main/views.py
+++ main/views.py
@@ -1,9 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import ProductForm
+from main.models import Product
+
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_main(request):
+    product_entries = Product.objects.all()
+
     return render(request, 'main.html', {
-        'application_name': 'Living Spaces',
         'name': 'Reyvano Mario Sianturi',
-        'class': 'PBP E'
+        'class': 'PBP E',
+        'product_entries': product_entries
     })
+
+def create_product_entry(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product_entry.html", context)
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(requst, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+
+def show_json_by_id(request):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
